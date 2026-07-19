656f2533beebd61c07cf29d3cd3caf200c1f2715
abdullahsrk16-cloud <abdullah.srk16@gmail.com>
2026-07-19 15:58:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const button = document.querySelector(".hero button");
+document.addEventListener("DOMContentLoaded", () => {
 
-    button.addEventListener("click", function () {
-        alert("Welcome to Rawdha Fashion Store! 🛍️");
+    // Hero Button
+    const shopBtn = document.querySelector(".shop-btn");
+
+    if (shopBtn) {
+        shopBtn.addEventListener("click", () => {
+            document.querySelector(".products").scrollIntoView({
+                behavior: "smooth"
+            });
+        });
+    }
+
+    // Add to Cart Buttons
+    const buttons = document.querySelectorAll(".product button");
+
+    buttons.forEach(btn => {
+        btn.addEventListener("click", () => {
+            btn.innerText = "✓ Added";
+            btn.style.background = "#28a745";
+
+            setTimeout(() => {
+                btn.innerText = "Add to Cart";
+                btn.style.background = "gold";
+            }, 2000);
+        });
     });
+
 });
