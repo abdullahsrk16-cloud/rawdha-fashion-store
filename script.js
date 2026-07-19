48fa38375a668ad30254f096c71c787fc50d75ff
abdullahsrk16-cloud <abdullah.srk16@gmail.com>
2026-07-19 16:37:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,48 @@
-document.addEventListener("DOMContentLoaded", () => {
+let cart = [];
 
-    // Hero Button
-    const shopBtn = document.querySelector(".shop-btn");
-
-    if (shopBtn) {
-        shopBtn.addEventListener("click", () => {
-            document.querySelector(".products").scrollIntoView({
-                behavior: "smooth"
-            });
-        });
-    }
-
-    // Add to Cart Buttons
-    const buttons = document.querySelectorAll(".product button");
-
-    buttons.forEach(btn => {
-        btn.addEventListener("click", () => {
-            btn.innerText = "✓ Added";
-            btn.style.background = "#28a745";
-
-            setTimeout(() => {
-                btn.innerText = "Add to Cart";
-                btn.style.background = "gold";
-            }, 2000);
-        });
+function addToCart(name, price) {
+    cart.push({
+        name: name,
+        price: price
     });
 
-});
+    updateCart();
+
+    alert(name + " added to cart!");
+}
+
+function updateCart() {
+    const cartCount = document.getElementById("cart-count");
+    const cartItems = document.getElementById("cart-items");
+    const total = document.getElementById("cart-total");
+
+    if (!cartCount || !cartItems || !total) return;
+
+    cartCount.innerText = cart.length;
+
+    cartItems.innerHTML = "";
+
+    let sum = 0;
+
+    cart.forEach(item => {
+        sum += item.price;
+
+        cartItems.innerHTML += `
+            <li>
+                ${item.name} - $${item.price}
+            </li>
+        `;
+    });
+
+    total.innerText = "$" + sum;
+}
+
+function toggleCart() {
+    const panel = document.getElementById("cart-panel");
+
+    if (panel.style.right === "0px") {
+        panel.style.right = "-400px";
+    } else {
+        panel.style.right = "0px";
+    }
+}
