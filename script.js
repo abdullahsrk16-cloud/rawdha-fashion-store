57c8930d5ea3dda867e30c73463cc8b3ec72f9a5
abdullahsrk16-cloud <abdullah.srk16@gmail.com>
2026-07-20 15:11:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -87,3 +87,22 @@ function checkout() {
 }
 
 updateCart();
+function searchProducts() {
+
+    const input = document.getElementById("search").value.toLowerCase();
+
+    const products = document.querySelectorAll(".product");
+
+    products.forEach(product => {
+
+        const name = product.querySelector("h3").innerText.toLowerCase();
+
+        if (name.includes(input)) {
+            product.style.display = "block";
+        } else {
+            product.style.display = "none";
+        }
+
+    });
+
+}
